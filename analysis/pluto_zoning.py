169d1abf191a17f79fc4b375b3208320c0815192
ziggy <ziggy@elephant-bird.net>
2016-05-02 13:14:10 -0400

    Add doc strings

--- analysis/pluto_zoning.py
+++ analysis/pluto_zoning.py
@@ -9,6 +9,9 @@ versions = ('03c', '04c','05d', '06c', '07c', '09v2', '10v2', '11v2', '12v2', '1
 
 
 def select(query):
+    """
+    Executes the query and returns the data as a list of tuples.
+    """
     cur.execute(query)
     return cur.fetchall()
 
@@ -18,19 +21,32 @@ def make_query(ver, cols):
 
 
 def db_to_df(ver, cols):
+    """
+    Creates DataFrame for the pluto verion with the given columns.
+    """
     return pd.DataFrame(select(make_query(ver, cols)), columns=cols)
 
 
 def zonetype(df):
+    """
+    Add new column, zonetype, which is the first letter of zonedist1.
+    """
     df['zonetype'] = df['zonedist1'].str.slice(start=0, stop=1)
     return df
 
 
 def group_by_cd_zonetype(df):
+    """
+    Groups the dataframe by CD and Zonetype, sums up the other fields, and swaps the axis.
+    """
     return df.groupby(['cd', 'zonetype']).sum().unstack(1)
 
     
 def calc_percents(df):
+    """
+    Cacluates the R and M zoning percents. 
+    Assumes the only original numeric columns was lotarea.
+    """
     df['totalarea'] = df.sum(1)
     df['R_pct'] = df[('lotarea', 'R')] / df['totalarea']
     df['M_pct'] = df[('lotarea', 'M')] / df['totalarea']
@@ -38,6 +54,10 @@ def calc_percents(df):
 
 
 def rename_pct_cols(df, ver):
+    """
+    Renames the percent columns to be prefaced with the version.
+    I.e. R_pct becomes 15v1_R_pct
+    """
     return df.rename(columns={'R_pct': ver + '_R_pct', 'M_pct': ver + '_M_pct'})
 
 
@@ -53,6 +73,9 @@ def keep_only(df, ver, cols):
 
 
 def df_for_ver(ver):
+    """
+    Returns a processed DataFrame for the pluto version.
+    """
     rename = partial(rename_pct_cols, ver=ver)
     keep_only_ = partial(keep_only, ver=ver, cols=['R_pct', 'M_pct'])
     funcs = (zonetype, group_by_cd_zonetype, calc_percents, rename, keep_only_)
