27a652f846b7d8001d915bc23b6d31d799659267
ziggy <zig@riseup.net>
2016-12-04 15:49:46 -0500

    use pypi version of pyproj (instead of compiling it)

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 python3 -m venv venv
 source venv/bin/activate
+pip3 install pyproj 
 
-pip3 install cython
-
-git clone https://github.com/jswhit/pyproj.git
-cd pyproj
-python3 setup.py build
-python3 setup.py install
+# pip3 install cython
+# git clone https://github.com/jswhit/pyproj.git
+# cd pyproj
+# python3 setup.py build
+# python3 setup.py install
